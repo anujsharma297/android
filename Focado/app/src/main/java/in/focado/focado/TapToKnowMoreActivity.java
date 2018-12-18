464d76e21519ada80e144545a6937bd4e43ea7fa
anujsharma297 <vasusharma.in@gmail.com>
2018-12-18 19:14:28 +0530

    New Version

--- Focado/app/src/main/java/in/focado/focado/TapToKnowMoreActivity.java
+++ Focado/app/src/main/java/in/focado/focado/TapToKnowMoreActivity.java
@@ -3,11 +3,19 @@ package in.focado.focado;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class TapToKnowMoreActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+
         setContentView(R.layout.activity_tap_to_know_more);
     }
 }
